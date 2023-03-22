1a4dcf6f15c6d6f658db4be7bb0adb54f00f2009
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-03-22 09:26:42 +0900

    mod: [quiz] quiz cardのUI改善

--- src/features/Review/Quiz.jsx
+++ src/features/Review/Quiz.jsx
@@ -1,28 +1,51 @@
-import { CheckIcon, SmallCloseIcon } from '@chakra-ui/icons';
+import { CheckCircleIcon, CheckIcon, SmallCloseIcon } from '@chakra-ui/icons';
 import { Card, Heading, CardBody, Stack, Text, Divider, CardFooter, ButtonGroup, Button } from '@chakra-ui/react';
 import React from 'react';
 
 // TODO: stageを表示
 // TODO: 要素間のスペースを作る
 
-const Quiz = ({ activeQuiz }) => {
+const Quiz = ({ activeQuiz, answerCorrect, answerIncorrect }) => {
   return (
     <>
       <Card maxW="sm">
         <CardBody>
           <Stack mt="6" spacing="3">
-            <Heading size="md">{activeQuiz.quiz}</Heading>
+            <Heading size="md" position="relative">
+              {activeQuiz.quiz}
+              <CheckCircleIcon
+                w={3}
+                h={3}
+                position="absolute"
+                top="4px"
+                left="-16px"
+                color={activeQuiz.isAnswered ? 'teal.500' : 'gray.200'}
+              />
+            </Heading>
             <Heading size="xs">答え</Heading>
             <Text>{activeQuiz.answer}</Text>
+            <Text>{activeQuiz.bookTitle}</Text>
           </Stack>
         </CardBody>
         <Divider />
         <CardFooter>
           <ButtonGroup spacing="2" justifyContent={'space-between'} w="100%">
-            <Button rightIcon={<SmallCloseIcon />} colorScheme="teal" variant="outline" flexBasis={'50%'}>
+            <Button
+              rightIcon={<SmallCloseIcon />}
+              colorScheme="teal"
+              variant="outline"
+              flexBasis={'50%'}
+              onClick={() => answerIncorrect()}
+            >
               わからなかった
             </Button>
-            <Button rightIcon={<CheckIcon />} colorScheme="teal" variant="solid" flexBasis={'50%'}>
+            <Button
+              rightIcon={<CheckIcon />}
+              colorScheme="teal"
+              variant="solid"
+              flexBasis={'50%'}
+              onClick={() => answerCorrect()}
+            >
               わかった
             </Button>
           </ButtonGroup>
