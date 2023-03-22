2f6fa79df78736e9c7372fe01e8623384766073f
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-03-22 09:28:18 +0900

    add:[quiz] 回答する時のロジックをコメントで追記

--- src/features/Review/Top.jsx
+++ src/features/Review/Top.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ButtonBack } from '../../components/ButtonBack';
-import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
+import { Button, CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
 import Quiz from './Quiz';
 import AnswerButtons from './AnswerButtons';
+import { update } from '../../store/modules/quizSlice';
 
 const ReviewRoot = ({ backToTop }) => {
+  const dispatch = useDispatch();
   const { status, quizList } = useSelector((state) => state.quiz);
 
   // 今日の日付も含めて、今日よりも前か判定する関数
@@ -22,34 +24,97 @@ const ReviewRoot = ({ backToTop }) => {
   };
 
   // 今日復習するquizの配列を生成
-  // 今日より前に復習日だった問題を格納
+  // 今日より前に復習日だった問題を格納=今日回答するべきクイズ
   const reviewQuizList = quizList.filter((quiz) => isBeforeToday(quiz.reviewDate));
 
   // クイズリストから、回答済みのクイズ
-  const answeredQuizList = reviewQuizList.filter((quiz) => !quiz.isAnswered);
-  const [quizIndex, setQuizIndex] = useState(reviewQuizList.length - answeredQuizList.length);
-  const [showQuizIndex, setShowQuizIndex] = useState(quizIndex);
+  const answeredQuizList = reviewQuizList.filter((quiz) => quiz.isAnswered);
 
-  const [activeQuiz, setActiveQuiz] = useState(quizList[quizIndex]);
+  // 回答済みクイズの個数
+  // state更新したら、自動で更新される？
+  // そうしたら、answeredQuizをuseStateで管理する必要なくなる？
+  const [answeredQuiz, setAnsweredQuiz] = useState(answeredQuizList.length);
+
+  // 表示しているクイズのindex
+  // 未回答の回答の最初のクイズにする
+  // state更新したら、自動で更新される？
+  // そうしたら、answeredQuizをuseStateで管理する必要なくなる？
+  const [showQuizIndex, setShowQuizIndex] = useState(answeredQuiz);
+
+  // 表示しているクイズ
+  const [activeQuiz, setActiveQuiz] = useState(reviewQuizList[showQuizIndex]);
+
+  // console.log(answeredQuizList);
+  // console.log({ answeredQuiz });
+  // console.log({ showQuizIndex });
+  // console.log(activeQuiz);
+
+  const answerCorrect = () => {
+    // update();
+    // 表示しているクイズが回答済
+    // --未回答のクイズあり
+    // ----未回答のクイズのインデックスに遷移
+    // ----回答済みクイズの個数はそのまま
+    // --未回答のクイズなし
+    // ----showQuizIndex,answeredQuizそのまま
+    // ----回答済みクイズの個数はそのまま
+
+    // 表示しているクイズが未回答
+    // --未回答のクイズあり
+    // ----未回答のクイズのインデックスに遷移
+    // --未回答のクイズなし
+    // ----showQuizIndex,answeredQuizそのまま
+    // --未回答のクイズのインデックスに遷移
+    // --回答済みクイズの個数を+1
+
+    // if (answeredQuiz < reviewQuizList.length) {
+    //   if (showQuizIndex < reviewQuizList.length) {
+    //     changeNextQuiz();
+    //     setAnsweredQuiz((prev) => prev + 1);
+    //   } else {
+    //   }
+    // }
+    const updatedQuiz = { ...activeQuiz };
+    // isAnsweredをtrueに更新
+    updatedQuiz.isAnswered = true;
+    // stage操作
+    dispatch(update(updatedQuiz));
+  };
+
+  const resetIsAnswered = () => {
+    console.log('reset');
+  };
+
+  const answerIncorrect = () => {
+    if (answeredQuiz < reviewQuizList.length) {
+      changeNextQuiz();
+      setAnsweredQuiz((prev) => prev + 1);
+    }
+  };
 
   // 次のクイズ
   const changeNextQuiz = () => {
+    if (showQuizIndex > reviewQuizList.length) return;
     setShowQuizIndex((prev) => prev + 1);
+    setActiveQuiz(reviewQuizList[showQuizIndex]);
   };
 
   // 前のクイズ
   const changePrevQuiz = () => {
     if (showQuizIndex < 0) return;
     setShowQuizIndex((prev) => prev - 1);
+    setActiveQuiz(reviewQuizList[showQuizIndex - 1]);
   };
 
+  console.log(reviewQuizList);
   return (
     <>
       <ButtonBack label="TOP" cb={backToTop} />
+      <Button onClick={resetIsAnswered}>reset</Button>
 
-      <Quiz activeQuiz={activeQuiz} />
+      <Quiz activeQuiz={activeQuiz} answerCorrect={answerCorrect} answerIncorrect={answerIncorrect} />
       <AnswerButtons
-        quizIndex={quizIndex}
+        quizIndex={answeredQuiz}
         showQuizIndex={showQuizIndex}
         totalReviewQuiz={reviewQuizList.length}
         changePrevQuiz={changePrevQuiz}
